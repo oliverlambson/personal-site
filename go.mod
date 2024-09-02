57f79a13a175f38e66060c55b65c9365ad26d2b9
Oliver Lambson <oliverlambson@gmail.com>
2024-09-02 11:34:57 +0200

    highlighting wip

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/oliverlambson/personal-site
 go 1.23
 
 require github.com/gomarkdown/markdown v0.0.0-20240730141124-034f12af3bf6
+
+require (
+	github.com/alecthomas/chroma v0.10.0 // indirect
+	github.com/dlclark/regexp2 v1.4.0 // indirect
+)
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -8,8 +9,13 @@ import (
 	"strings"
 	"time"
 
+	"github.com/alecthomas/chroma"
+	"github.com/alecthomas/chroma/formatters/html"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
 	"github.com/gomarkdown/markdown"
-	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/ast"
+	mdhtml "github.com/gomarkdown/markdown/html"
 	"github.com/gomarkdown/markdown/parser"
 )
 
@@ -39,35 +45,87 @@ func loggingMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func MdToHTML(md []byte) []byte {
-	// see: https://blog.kowalczyk.info/article/cxn3/advanced-markdown-processing-in-go.html
+// factory so i don't need a global htmlFormatter var
+func makeHTMLHighligher(htmlFormatter *html.Formatter, highlightStyle *chroma.Style) func(w io.Writer, source, lang, defaultLang string) error {
+	return func(w io.Writer, source, lang, defaultLang string) error {
+		if lang == "" {
+			lang = defaultLang
+		}
+		l := lexers.Get(lang)
+		if l == nil {
+			l = lexers.Analyse(source)
+		}
+		if l == nil {
+			l = lexers.Fallback
+		}
+		l = chroma.Coalesce(l)
+
+		it, err := l.Tokenise(nil, source)
+		if err != nil {
+			return err
+		}
+
+		return htmlFormatter.Format(w, highlightStyle, it)
+	}
+}
+
+// factory so i don't need a global htmlHighlight var
+func makeCodeBlockRenderHook(htmlHighlight func(w io.Writer, source, lang, defaultLang string) error) func(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
+	return func(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
+		if code, ok := node.(*ast.CodeBlock); ok {
+			defaultLang := ""
+			lang := string(code.Info)
+			htmlHighlight(w, string(code.Literal), lang, defaultLang)
+			return ast.GoToNext, true
+		}
+		return ast.GoToNext, false
+	}
+}
+
+// see: https://blog.kowalczyk.info/article/cxn3/advanced-markdown-processing-in-go.html
+func mdToHTML(md []byte) []byte {
 	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
 	p := parser.NewWithExtensions(extensions)
 	doc := p.Parse(md)
 
-	htmlFlags := html.CommonFlags | html.HrefTargetBlank
-	opts := html.RendererOptions{Flags: htmlFlags}
-	renderer := html.NewRenderer(opts)
-
+	highlightStyle := styles.Get("monokailight")
+	formatter := html.New(html.WithClasses(true), html.TabWidth(2))
+	// TODO: formatter.WriteCSS(w io.Writer, style *chroma.Style)
+	htmlHighlight := makeHTMLHighligher(formatter, highlightStyle)
+	codeBlockRenderHook := makeCodeBlockRenderHook(htmlHighlight)
+	opts := mdhtml.RendererOptions{
+		Flags:          mdhtml.CommonFlags | mdhtml.HrefTargetBlank,
+		RenderNodeHook: codeBlockRenderHook,
+	}
+	renderer := mdhtml.NewRenderer(opts)
 	return markdown.Render(doc, renderer)
 }
 
 func markdownHandler(w http.ResponseWriter, r *http.Request) {
+	htmlFormatter := html.New(html.WithClasses(true), html.TabWidth(2))
+	if htmlFormatter == nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Println("couldn't create html formatter")
+		return
+	}
+
 	filePath := filepath.Join("web/content/in/", strings.TrimPrefix(r.URL.Path, "/content/in/"))
 
 	md, err := os.ReadFile(filePath)
 	if err != nil {
 		http.Error(w, "File not found", http.StatusNotFound)
+		log.Printf("failed to read file: %v", err)
 		return
 	}
 
-	html := MdToHTML(md)
+	html := mdToHTML(md)
 
 	w.Header().Set("Content-Type", "text/html")
-	w.Write(html)
-	// TODO: add title from markdown frontmatter
-	// TODO: add style.css
-	// TODO: add code highlighting
+	_, err = w.Write(html)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("failed to write response: %v", err)
+	}
 }
 
 func NewServer(Addr string) *http.Server {
