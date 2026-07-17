394c75784439c0e309c335037b7023084f83d2c0
Oliver Lambson <oliverlambson@gmail.com>
2026-07-17 17:01:47 -0400

    remove retired container deployment (#10)

--- deployment/local/compose-activate.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/env/bin bash
-
-export COMPOSE_FILE=$(find deployment -type f -name 'compose*.yaml' | tr '\n' ':' | sed 's/:$//')
-export SHA=$(git rev-parse --short HEAD)
-export VERSION=latest
